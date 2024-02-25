b3541b7ef7b273dd93fd0404116cc4cfb858ef5e
DavAnders <davidanderson7421@gmail.com>
2024-02-25 01:33:16 -0600

    add html and update server function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
